cf1289f1d1108e1d179e69244437e86173f87080
Héctor Esquivel <hectorsqvl@gmail.com>
2025-08-17 17:33:34 -0600

    Implementada la función agregar amigos con respectivas validaciones. Funcionando

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaNombres = [];
+
+function agregarAmigo(){
+    let aux = document.getElementById('amigo');
+    let nombre = aux.value;
+    aux.value = "";
+    console.log(nombre);
+    nombre = nombre.trim();
+    console.log(`El dato ingresado es ${nombre}`);
+    if(validaNombre(nombre)){
+        listaNombres.push(nombre);        
+        aux.placeholder = "Agrega otro nombre";
+        console.log("Agregar ejecutado.");
+    }
+    return;
+}
+
+function validaNombre(nombre){
+    //let n = nombre.trim();
+    console.log(`Dato en validación "${nombre}"`);
+    if(nombre != ""){
+        return true;
+    }else{
+        alert("Por favor, inserte un nombre.")
+        return false;
+    }
+}
