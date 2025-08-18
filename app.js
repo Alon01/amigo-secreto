07a8a6cd5fcfb3ab7a6228e17ed9db4834d365ea
Héctor Esquivel <hectorsqvl@gmail.com>
2025-08-18 13:26:55 -0600

    Se agregan las funciones para agregar a pantalla la lista y otra funcion para validar que no hay duplicados.

--- app.js
+++ app.js
@@ -10,27 +10,55 @@ function agregarAmigo(){
     aux.value = "";
     console.log(nombre);
     nombre = nombre.trim();
-    console.log(`El dato ingresado es ${nombre}`);
-    if(validaNombre(nombre)){
+    if(validaNombre(nombre) && validaDuplicados(nombre)){
         listaNombres.push(nombre);        
         aux.placeholder = "Agrega otro nombre";
-        console.log("Agregar ejecutado.");
+        recorreAgrega();
     }
     return;
 }
-
 /**
  * Valida que sea un string y no solo espacios o un vacio.
  * @param {String} nombre 
  * @returns Devuelve un true o false según sea el caso.
  */
 function validaNombre(nombre){
-    //let n = nombre.trim();
-    console.log(`Dato en validación "${nombre}"`);
     if(nombre != ""){
         return true;
     }else{
         alert("Por favor, inserte un nombre.")
         return false;
+    }
+}
+/**
+ * Agrega a pantalla los datos ingresados.
+ * @returns No devuelve nada.
+ */
+function recorreAgrega(){
+    const lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+    for (let index = 0; index < listaNombres.length; index++) {
+        lista.innerHTML += `<li>${listaNombres[index]}</li>`;
+    }
+    return;
+}
+/**
+ * Valida si el dato ingresano no es duplicado.
+ * @param {String} nombre 
+ * @returns Devuelve booleano confirmando a negando que el dato no es duplicado.
+ */
+function validaDuplicados(nombre){
+    if (listaNombres.length > 0) {
+        for (let index = 0; index < listaNombres.length; index++) {
+            if (nombre === listaNombres[index]){
+                alert("Nombre duplicado, ingresa otro.");
+                return false;
+            }
+            else{
+                return true;
+            }        
+        }        
+    }else{
+        return true;
     }
 }
